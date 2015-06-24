d4debd63c9635c42074a99854eb725882fe0b7f6
Millard Frédéric <fmi@octo.com>
2015-06-24 17:22:43 +0200

    Implémentation du dispatcher

--- main.hs
+++ main.hs
@@ -5,12 +5,32 @@ import Data.List
 -- getProgName :: IO String
 -- putStrLn :: String -> IO ()
 -- mapM :: Monad m => (a -> m b) -> [a] -> m [b]
+-- lookup :: Eq a => a -> [(a, b)] -> Maybe b
+
+type Args = [String]
+type CommandeName = String 
+
+helloCmd :: Args -> IO ()
+helloCmd _ = putStrLn "Hello world"
+
+pingCmd :: Args -> IO ()
+pingCmd _ = putStrLn "pong"
+
+helpCmd :: Args -> IO ()
+helpCmd _ = do
+	putStrLn "Les commandes fmi-cmd :"
+	putStrLn "      hello       Affiche \"Hello World\""
+	putStrLn "      ping        Affiche \"pong\""
+	putStrLn "      help        Affiche l'aide"
+
+dispatch :: [ (CommandeName, Args -> IO ()) ]
+dispatch = [ ("hello", helloCmd)
+           , ("ping", pingCmd) 
+           , ("help", helpCmd) 
+           ]
 
 main :: IO ()
 main = do
-	args <- getArgs
-	progName <- getProgName
-	putStrLn "Les arguments sont "
-	mapM putStrLn args
-	putStrLn "Le nom du programme est "
-	putStrLn progName
+	(cmdName:args) <- getArgs
+	let (Just cmd) = lookup cmdName dispatch
+	cmd args
